c86e43642a6c90598c009e6fbc09c99a5b0870ad
MarianoAstorga <123986433+MarianoAstorga@users.noreply.github.com>
2025-11-19 21:08:37 -0300

    Delete TrabajoIntegrador directory

--- TrabajoIntegrador/Proyecto/Proyecto/src/main/java/com/tupad/app/Main.java
+++
@@ -1,7 +0,0 @@
-package com.tupad.app;
-
-public class Main {
-    public static void main(String[] args) {
-        new AppMenu().start();
-    }
-}
--- TrabajoIntegrador/Proyecto/Proyecto/src/main/java/com/tupad/dao/FichaBibliograficaDao.java
+++
@@ -1,5 +0,0 @@
-package com.tupad.dao;
-
-import com.tupad.entities.FichaBibliografica;
-
-public interface FichaBibliograficaDao extends GenericDao<FichaBibliografica> {}
